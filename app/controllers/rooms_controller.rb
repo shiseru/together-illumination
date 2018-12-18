ede21819f70ca1b92629bb0675d3522f1c259a32
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-18 16:34:25 +0900

    roomに参加できるのは投稿者と返信者だけにする

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,6 +10,12 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    room = Room.find(params[:id])
+    if room.is_authorized?(current_user.id) then
+      @messages = Message.where(room_id: params[:id]).order(created_at: :asc)
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
   # GET /rooms/new
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,10 @@ class Room < ApplicationRecord
   belongs_to :post
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   belongs_to :participant, class_name: 'User', foreign_key: 'participant_id'
+
+  has_many :messages
+
+  def is_authorized?(user_id)
+    return owner_id == user_id || participant_id == user_id
+  end
 end
