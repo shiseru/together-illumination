6fbd4abe2b2a78354ae2f0c5653eb890bed63e88
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-18 16:16:58 +0900

    Model Messageを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_070835) do
+ActiveRecord::Schema.define(version: 2018_12_18_071333) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
