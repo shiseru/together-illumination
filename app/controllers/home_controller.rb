472e0e407ed96e1e6f1914ea9fc9beeaa2a08a2c
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-18 17:57:17 +0900

    ビューと語句を調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,8 @@ class HomeController < ApplicationController
   def about
 
   end
+
+  def mypage
+    @rooms = Room.where(owner_id: current_user.id).or(Room.where(participant_id: current_user.id))
+  end
 end
