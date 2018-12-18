af7fe6e4c9279c225f269eef5794354ab30d9ef1
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-18 16:16:34 +0900

    Model Messageを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # post model
   has_many :posts
+
+  # room model
   has_many :owner_rooms, class_name: 'Room', foreign_key: 'owner_id'
   has_many :participant_rooms, class_name: 'Room', foreign_key: 'participant_id'
+
+  # message model
+  has_many :messages
 end
