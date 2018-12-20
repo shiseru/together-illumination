5d9a51b6582c007d4dcd7b48dee74d13cbddc748
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-21 06:12:50 +0900

    管理画面(admin page)を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   resources :rooms#, only: [:show, :destroy]
   post 'rooms/:id/create_message' => 'rooms#create_message'
