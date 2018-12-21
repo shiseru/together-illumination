9259cd469514312fa8cdbb21c6c07e1a09b58806
Shisei Naka <nathaniel.naka@gmail.com>
2018-12-21 21:52:32 +0900

    ルートを整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  get 'illuminations/update_db'
+  get '/' => 'home#top'
+  get '/home/show' => 'home#show'
+  get '/about' => 'home#about'
+  get '/mypage' => 'home#mypage'
+  get '/update_db' => 'illuminations#update_db'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
 
-  resources :rooms#, only: [:show, :destroy]
+  resources :rooms, only: [:show, :destroy]
   post 'rooms/:id/create_message' => 'rooms#create_message'
   post 'rooms/:owner_id/:participant_id/:post_id/create' => 'rooms#create'
 
   resources :posts
 
-  get '/' => 'home#top'
-  get '/home/show' => 'home#show'
-  get '/about' => 'home#about'
-  get '/mypage' => 'home#mypage'
-
-  get '/update_db' => 'illuminations#update_db'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 end
